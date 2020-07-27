85dc2871c955d126a8c3cebe03c6efddd7e4a4c9
Aastha jain <aasthajain0007.aj@gmail.com>
2020-07-27 15:47:56 +0530

    server fileupadted

--- src/server.js
+++ src/server.js
@@ -13,26 +13,3 @@ app.listen(process.env.PORT || 3000,function(){
     console.log("server is running on port 3000")
 })
 
-// app.get("/",function(req,res){
-//     res.sendFile(__dirname+"/signup.html")
-
-// })
-
-// app.post("/",function(req,res){
-    
-//     // form name is use here
-//     var fname =req.body.firstname
-//     var lname =req.body.secondname
-//     var email =req.body.emailid
-//     console.log(req.body.firstname)
-//     console.log(req.body.secondname)
-//     console.log(req.body.emailid)
-// })
-
-
-//API KEY
-// a69bf5d23817fdbd7517538d3a095ebb-us10
-
-
-// audience/list id 
-//82e9ee67ea
