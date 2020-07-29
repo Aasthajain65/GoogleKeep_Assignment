5b7ca25f1da40ca1df6701a42ff14d9899776ce7
Aastha jain <aasthajain0007.aj@gmail.com>
2020-07-29 16:32:37 +0530

    s

--- src/Component/Newheader.js
+++ src/Component/Newheader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import AppBar from "@material-ui/core/AppBar";
+// import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
